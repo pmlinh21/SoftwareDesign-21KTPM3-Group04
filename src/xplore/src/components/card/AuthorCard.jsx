d22d70dccb48f6868178b63b523a30efae25142f
NgoNgocLien <nnlien21@clc.fitus.edu.vn>
2024-03-17 13:25:47 +0700

    Move button styles

---
+++ src/xplore/src/components/card/AuthorCard.jsx
@@ -0,0 +1,13 @@
+function AuthorCard({ author }) {
+  return (
+    <div className="author-card">
+      {/* <div className="author-card__avatar">
+        <img src={author.avatar} alt={author.name} />
+      </div>
+      <div className="author-card__info">
+        <h3>{author.name}</h3>
+        <p>{author.bio}</p>
+      </div> */}
+    </div>
+  )
+}
