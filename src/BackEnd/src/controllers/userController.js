3649df8174ddcdadda494206336254f8b8d92979
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2024-03-23 00:55:55 +0700

    add user controller login and signup through email and pwd function

--- src/BackEnd/src/controllers/userController.js
+++ src/BackEnd/src/controllers/userController.js
@@ -5,17 +5,64 @@ const model = init_models(sequelize);
 
 const { successCode, failCode, errorCode } = require('../config/response');
 
-const get = async (req,res) => {
+// GET: Login
+const login = async(req, res) =>{
+    let { email, pwd } = req.body
+
     try{
-        successCode(res,null," found")
+        let user = await model.user.findOne({
+            where: {
+                email
+            }
+        });
+
+        if (user){
+            //let checkPass = bcrypt.compareSync(pwd, user.password);
+            if(pwd === user.password){
+                successCode(res, user, "Login successfully");
+                return;
+            }
+            // if(checkPass){
+            //     user.password = '**********';
+            //     sucessCode(res, user, "Login successfully");
+            //     return;
+            // }
+            else{
+                failCode(res, "", "Email or password wrong");
+                return;
+            }
+        } 
+
+    }catch(err){
+        console.log(err)
+        errorCode(res,"Internal Server Error")
+    }
+}
+
+// POST: Signup
+const signup = async(req, res) =>{
+    try{
+        let { email, password } = req.body;
+        let checkEmail = await model.user.findOne({
+            where:{
+                email
+            }
+        })
+        if(checkEmail){
+            failCode(res,"","Email is existing");
+            return;
+        }
+        else{
+            let passWordHash = bcrypt.hashSync(password, 10);
+            let data = await model.user.create({ email, password: passWordHash });
+            successCode(res, data, "Sign up successfully");
+            return;
+        }
     }
     catch(err){
         console.log(err)
         errorCode(res,"Internal Server Error")
     }
-
 }
 
-module.exports = {
-
-}
+module.exports = { login, signup }
--- src/BackEnd/src/routes/userRoute.js
+++ src/BackEnd/src/routes/userRoute.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const userRoute = express.Router();
+const { login, signup } = require("../controllers/userController")
 
+// GET: Login
+userRoute.get("/login", login)
+
+// POST: Signup
+userRoute.post("/signup", signup)
 
 module.exports = userRoute;
