f2c5ac56f93f18e69406904539f360310ddf108a
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2024-04-05 08:53:54 +0700

    modify token

--- src/BackEnd/src/middlewares/baseToken.js
+++ src/BackEnd/src/middlewares/baseToken.js
@@ -2,14 +2,14 @@ const jwt = require('jsonwebtoken');
 
 // Create a new JWT based on the provided payload data
 const parseToken = (data) => {
-    let token = jwt.sign({ data }, "xplore", { algorithm: 'HS256', expiresIn: "10y" });
+    let token = jwt.sign({ data }, process.env.JWT_SECRET_KEY, { algorithm: 'HS256', expiresIn: "10y" });
     return token;
 }
 
 // Verify the validity of a given JWT
 const checkToken = (token) => {
     try {
-        let checkT = jwt.verify(token, "xplore");
+        let checkT = jwt.verify(token, process.env.JWT_SECRET_KEY);
         if (checkT) {
             return { checkData: true, messagse: "" };
         } else {
@@ -41,7 +41,7 @@ const decodeToken = (token) => {
     }
 
     try {
-        const decode = jwt.verify(token, "xplore");
+        const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
         return decode;
     } catch (error) {
         // Handle token verification error
