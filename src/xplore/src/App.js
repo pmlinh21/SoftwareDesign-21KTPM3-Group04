5802b94c9cccb599a2a1877815acd1d11ead0710
NgoNgocLien <nnlien21@clc.fitus.edu.vn>
2024-04-06 10:27:43 +0700

    commit to pull

--- src/xplore/src/App.js
+++ src/xplore/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import BlogCard from './components/blog-card/BlogCard';
+import Navbar from './components/navbar/Navbar';
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
+    // <Router>
+    //     <div className="App">
+    //         <Nav />
+    //         <Routes>
+    //             <Route path="/" element={<Home />} />
+    //             <Route path="/tweets" element={<Tweet />} />
+    //         </Routes>
+    //     </div>
+    // </Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar />
+        <Home />
     </div>
   );
 }
