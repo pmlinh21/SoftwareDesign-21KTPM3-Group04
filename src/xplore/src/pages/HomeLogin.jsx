9bfeb8a0441a32be553271d051f4394e0b07e937
FelinTran <felintran.work@gmail.com>
2024-04-18 14:56:39 +0700

    homelogin

--- src/xplore/src/pages/HomeLogin.jsx
+++ src/xplore/src/pages/HomeLogin.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "../styles/commons.css";
 import "./SearchResult.css"
 import Search from '../components/search/Search'
+import BlogPostCard from '../components/blog-card/BlogPostCard'
 import TopicTag from '../components/topic/TopicTag'
 
 import { useState, useEffect, useRef } from 'react';
@@ -9,28 +10,27 @@ import {useSelector} from 'react-redux'
 import { useNavigate, useLocation   } from 'react-router-dom';
 import AuthorHorizontal from '../components/author-card/AuthorHorizontal';
 import BlogCardHorizontal from '../components/blog-card/BlogCardHorizontal';
-import Loading from '../components/loading/Loading';
 
 function HomeLogin() {
     return (
         <div>
             {/* Search bar */}
             <Search search="" isResult={false} />
-
-            <div className="container-fluid">
-                <div className="container">
-                    <div className="row g-4">
+            <div className='container-fluid'>
+                <div className='container'>
+                    <div className='row g-4'>
                         {/* Tab bar */}
-                        <div className="col-6">
-                            
+                        <div className='col-6'>
+                                    
                         </div>
                         {/* Trending */}
                         {/* Following */}
-                        {/* Topics */}
+                           {/* Topics */}
                     </div>
                 </div>
             </div>
         </div>
+        
     );
 }
 export default HomeLogin;
