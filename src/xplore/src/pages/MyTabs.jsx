f4ec74e77a2a7dac20e61dcedc3379ee8f1d6138
pmlinh21 <pmlinh21@clc.fitus.edu.vn>
2024-04-29 19:01:37 +0700

    update

--- src/xplore/src/pages/MyTabs.jsx
+++ src/xplore/src/pages/MyTabs.jsx
@@ -3,12 +3,14 @@ import {useSelector, useDispatch} from 'react-redux';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import { getTopicByUserAction } from '../redux/actions/UserAction';
+import {formatCapitalCase} from '../util/formatText'
 import Loading from '../components/loading/Loading';
 
 const MyTabs = () => {
   const dispatch = useDispatch();
   const [tabsData, setTabsData] = useState([]);
   const {topic, user_login} = useSelector(state => state.UserReducer);
+  const {topics} = useSelector(state => state.TopicReducer);
   const {loading} = useSelector(state => state.LoadingReducer);
 
   useEffect(() => {
@@ -30,12 +32,14 @@ const MyTabs = () => {
       // };
 
       // fetchPost();
-      const tabsData = [
-        { title: 'Tab 1', content: 'Content for Tab 1' },
-        { title: 'Tab 2', content: 'Content for Tab 2' },
-        { title: 'Tab 3', content: 'Content for Tab 3' },
-        { title: 'Tab 3', content: 'Content for Tab 3' },
-      ];
+
+      const followedTopic = topics.filter(item => topic.includes(item.id_topic))
+      const tabsData = followedTopic.map(item => {
+        return {
+          title: formatCapitalCase(item.topic),
+          content: `Content for ${item.topic}`
+        }
+      })
   
       setTabsData(tabsData)
     }
