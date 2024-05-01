261108dee203e6db81c0a8ae512be6f8329e5180
FelinTran <felintran.work@gmail.com>
2024-05-01 10:27:41 +0700

    done FE post detail

--- src/xplore/src/pages/PostDetail.jsx
+++ src/xplore/src/pages/PostDetail.jsx
@@ -17,6 +17,11 @@ function Post() {
     const [Topics, setTopics] = useState([]);
     const [Post, setPost] = useState([]);
 
+    useEffect(() => {
+        fetchTopics();
+        fetchPost();
+    }, []);
+
     const fetchTopics = async () => {
         try {
             const topics = [];
@@ -25,8 +30,7 @@ function Post() {
                 topics.push(result.data.content[i]);
             }
             setTopics(topics);
-        }
-        catch (error) {
+        } catch (error) {
             console.log("error", error.response);
         }
     }
@@ -41,12 +45,8 @@ function Post() {
         }
     };
 
-    useEffect(() => {
-        fetchTopics();
-        fetchPost();
-    }, []);
-
     console.log("Topics", Topics);
+    console.log("Post", Post);
 
     return (
         <div>
@@ -59,20 +59,53 @@ function Post() {
                         <div className='col-2'></div>
                         <div className='col-8'>
                             {/* Post Title */}
-                            <h4>The 2024 Software Architect or Solution Architect RoadMap</h4>
+                            <h4 style={{marginBottom: '1rem'}}>The 2024 Software Architect or Solution Architect RoadMap</h4>
                             {/* Post Description */}
                             <p className='description'>An illustrated guide to becoming a Software Architect in 2024 with links to relevant courses</p>
                             {/* Post Topics */}
                             <div className="d-flex flex-wrap gap-2">
-                                {Topics.map(topic => (
+                                {/* {Topics.map(topic => (
                                     <button key={topic.id} className="topic label2 capitalize">{topic.topic}</button>
-                                ))}
+                                ))} */}
+                                <button className='topic label 2'>Business</button>
                             </div>
+                            {/* Post Authors */}
+                            <hr/>
+                            <div className='d-flex' style={{gap: '16px', padding: '24px 0'}}>
+                                {/* Avatar */}
+                                <img src="/imgs/Avatar-6.svg" style={{height: '44px', width: '44px'}} />
+                                {/* Name */}
+                                <div className='d-flex flex-column'>
+                                    <p className='support' style={{ color: 'var(--scheme-sub-text)', marginBottom: '8px' }}>Posted by</p>
+                                    <p className='label1' style={{margin: '0', color: 'var(--scheme-text)' }}>Oliver Knight</p>
+                                </div>
+                                {/* Date */}
+                                <div className='d-flex flex-column'>
+                                    <p className='support' style={{ color: 'var(--scheme-sub-text)', marginBottom: '8px' }}>Date posted</p>
+                                    <p className='label1' style={{margin: '0', color: 'var(--scheme-text)' }}>July 14, 2023</p>
+                                </div>
+                                <button className='prim-btn btn-sm' style={{width: '117px'}}>Follow</button>
+                                {/* Post Actions */}
+                                <div className='d-flex gap-2' style={{margin: '0 0 0 14.8rem'}}>
+                                    <button id='like-btn'>
+                                        <i className="fa-regular fa-heart" style={{fontSize: '20px'}}></i> 115
+                                    </button>
+                                    <button id='comment-btn'>
+                                        <i className="fa-regular fa-message" style={{fontSize: '20px'}}></i> 24
+                                    </button>
+                                    <button id='save-btn'>
+                                        <i className="fa-regular fa-bookmark" style={{fontSize: '20px'}}></i>
+                                    </button>
+                                    <button id='share-btn'>
+                                        <i className="fa-regular fa-share-from-square" style={{fontSize: '20px'}}></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <hr/>
                         </div>
                         <div className='col-2'></div>
                     </div>
                 </div>
-                
             </div>
         </div>
     )
