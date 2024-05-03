3aa725240b00adaed56da6effba27aad9bee6ce6
FelinTran <felintran.work@gmail.com>
2024-05-03 15:06:29 +0700

    done 404 not found

--- src/xplore/src/components/system-feedback/NotFound.jsx
+++ src/xplore/src/components/system-feedback/NotFound.jsx
@@ -1,9 +1,17 @@
 import './NotFound.css'
+import { useNavigate, Link } from 'react-router-dom'
 
 export default function NotFound({type}){
+    const navigate = useNavigate();
+    const handleNavigateHome = () => {
+        navigate("/home");
+    };
     return (
-        <div className="not-found-container col-12 d-flex align-items-center justify-content-center">
-            <p className="p1 text-scheme-sub-text not-found text-center ">404 NOT FOUND</p>
+        <div className="not-found-container col-12 d-flex flex-column gap-4 align-items-center justify-content-center">
+            <p className='subtitle1' style={{ color: 'var(--scheme-primary)', textAlign: 'center' }}>404 ERROR</p>
+            <h1>404</h1>
+            <p className='text-center p1' style={{  width: '20%'}}>Sorry, the page you are looking for doesn't exist. Here are what you can do:</p>
+            <button onClick={handleNavigateHome} className='prim-btn btn-md' style={{ width: '200px'}}>Back to Home</button>
         </div>
     )
 }
