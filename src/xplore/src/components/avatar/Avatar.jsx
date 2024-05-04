6d427b6404b40811fbdf8aa0b2acf66b95f97939
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2024-05-04 13:05:38 +0700

    add navigate to profile when select avatar

--- src/xplore/src/components/avatar/Avatar.jsx
+++ src/xplore/src/components/avatar/Avatar.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import "./Avatar.css";
 import avatarPlaceholder from "../../assets/images/avatar-placeholder.jpg"
+import { userService } from "../../services/UserService";
 
-export default function Avatar({avatar, size = "big"}) {
+export default function Avatar({avatar, id_user, size = "big"}) {
+
+    const navigate = useNavigate();
+
+    const handleAuthorClick = async () => {
+        try {
+            const author = await userService.getUserByID(id_user);
+            console.log(author)
+            if (author.status === 200) {
+                navigate("/author-profile", { state: { author: author.data.content } });
+            } else {
+                alert("Failed to block author");
+            }
+    
+        } catch (error) {
+            console.log("error", error);
+            alert("An error occurred while navigating to author profile");
+        }
+        
+    };
+
     return (
-        <img src={avatar || avatarPlaceholder} alt="user's avatar" className= {"avatar-container " + size} />
+        <img src={avatar || avatarPlaceholder} alt="user's avatar" className= {"avatar-container " + size} style={{cursor: "pointer"}} onClick={() => handleAuthorClick()}/>
     )
 }
